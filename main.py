8096c1d3340e082928f0871e6c25aef2146c65e8
phinh <phinh@ENDRR>
2025-03-09 21:33:54 +0700

    update full source code

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import pandas as pd
 from sqlServerConnection import SqlServerConnection
 
 # connect to sql server
-db = SqlServerConnection("ODBC Driver 17 for SQL Server", "DESKTOP-M0CSCJB\\SQLEXPRESS", "Pizza")
+db = SqlServerConnection("ODBC Driver 17 for SQL Server", ".\\SQLEXPRESS", "Pizza")
 conn = db.connection()
 
 # read xlsx file
 
 """
 Cài đặt thư viện pandas và nhúng openpyxl
-    pip install pandas openpyxl
+    pip install pandas 
+    pip intstall openpyxl
 """
 
 PATH = "Data.xlsx"
@@ -19,68 +20,27 @@ df = pd.read_excel(PATH, index_col=None)
 print(df.head())
 
 # data tables
-"""
-pizza_categories
-(
-    pizza_categories_id, 
-    pizza_category
-)
-"""
+"""------------------------------------------------------------------------------------------------------------------"""
 print('pizza_categories')
+category_columns = ('pizza_categories_id', 'pizza_category')
 
-# column
-pizza_category = df['pizza_category'].unique()
+pizza_category = db.import_data_to_dataframe(RAW_DATAFRAME=df, tuple_dataframe_columns=category_columns, the_firts_columns_is_null=True)
+# Lọc bo cac hang trung nhau dua tren cac gia tri trung nhau trong cot pizza_category
 
-# table: tạo dataFrame pizza_categories
-pizza_categories = pd.DataFrame({
-    'pizza_categories_id': range(1, len(pizza_category) + 1),
-    "pizza_category": pizza_category
-})
-
-print(pizza_categories)
+pizza_category = pizza_category.drop_duplicates(subset='pizza_category')
+print(pizza_category)
 
 
-"""
-pizzas
-(
-    pizza_id
-    pizza_name
-    pizza_size
-    unit_price
-    category_id
-    pizza_ingredients
-)
-"""
-print('pizzas')
+"""------------------------------------------------------------------------------------------------------------------"""
+# print('pizza')
 
-# column
-pizza_id = df['pizza_id']
-pizza_name = df['pizza_name']
-pizza_size = df['pizza_size']
-unit_price = df['unit_price']
-# cần gắn id thay vì tên
-pizza_category = df['pizza_category']
-pizza_ingredients = df['pizza_ingredients']
+pizza_column = ("pizza_id", "pizza_name", "pizza_size", "unit_price", "pizza_category", "pizza_ingredients")
+pizzas = db.import_data_to_dataframe(RAW_DATAFRAME=df, tuple_dataframe_columns=pizza_column)
 
-# table
-pizzas = pd.DataFrame({
-    'pizza_id' : pizza_id,
-    'pizza_name': pizza_name,
-    'pizza_size': pizza_size,
-    'unit_price' : unit_price,
-    'pizza_category' : pizza_category,
-    'pizza_ingredients' : pizza_ingredients
-})
-
-print(pizzas.head())
-
-"""
-Tạo khóa ngoại cho bảng pizza từ tên của pizza_categories
-"""
-# merge pizzas and pizza_categories
+# print(pizzas.head())
 
 pizzas = pizzas.merge(
-    pizza_categories,
+    pizza_category,
     how     ='left',
     left_on ='pizza_category',
     right_on='pizza_category'
@@ -88,86 +48,43 @@ pizzas = pizzas.merge(
 
 # drop tất cả cột pizza_category do lúc này đang merge 2 bang lai
 pizzas.drop(columns=['pizza_category'], inplace=True)
-
 # doi ten cot pizza_categories_id trong pizzas dataFrame sang pizza_category_id cho giong voi cot da ton tai trong csdl
 pizzas.rename(columns={'pizza_categories_id': 'pizza_category_id'}, inplace=True)
-
-print(len(pizzas))
-
-# table
 # gán kết qua drop_duplicates()
 pizzas = pizzas.drop_duplicates()
 
 print(pizzas)
 
-
-"""
-orders
-(
-    order_id
-    order_date
-    order_time
-    total_price
-    
-    order_detail_id
-)
-"""
-
+"""------------------------------------------------------------------------------------------------------------------"""
 print('orders')
-
-order_id    = df['order_id']
-order_date  = df['order_date']
-order_time  = df['order_time']
-total_price = 0
-# order_details_id = df['order_details_id']
-
-orders = pd.DataFrame({
-    'order_id' : order_id,
-    'order_date': order_date,
-    'order_time': order_time,
-    'total_price': total_price,
-})
+order_columns = ('order_id', 'order_date', 'order_time', 'total_price')
+orders = db.import_data_to_dataframe(df, order_columns, set_zero=True, column_set_zero='total_price')
 
 orders.drop_duplicates(subset='order_id', inplace=True)
-
 print(orders)
 
-"""
-order_details
-(
-    order_details_id
-    details
-    quantity
-    total_price
-    pizza_id
-)
-"""
-
+"""------------------------------------------------------------------------------------------------------------------"""
 print("order_details")
 
-order_details_id    = df['order_details_id']
-details             = df['details'] = 'null'
-quantity            = df['quantity']
-total_price         = df['total_price']
-pizza_id            = df['pizza_id']
-order_id            = df['order_id']
+order_detail_columns = ('order_details_id', 'details', 'quantity', 'total_price', 'pizza_id')
+order_details = db.import_data_to_dataframe(RAW_DATAFRAME=df, tuple_dataframe_columns=order_detail_columns, accept_null=True, column_accept_null='details')
 
-order_details = pd.DataFrame({
-    'order_details_id'  : order_details_id,
-    'details'           : details,
-    'quantity '         : quantity,
-    'total_price'       : total_price,
-    'pizza_id'          : pizza_id,
-    'order_id'          : order_id
-})
+print(order_details.head())
 
-print(order_details)
-
+"""------------------------------------------------------------------------------------------------------------------"""
 """
     Chuyển dữ liệu vào csdl
 """
 
-# db.import_data(pizza_categories, "pizza_categories")
-# db.import_data(pizzas, "pizzas")
-# db.import_data(orders, "orders")
+db.import_data(pizza_category, "pizza_categories")
+db.import_data(pizzas, "pizzas")
+db.import_data(orders, "orders")
 db.import_data(order_details, "order_details")
+
+# import csv
+#
+# demo_dataframe = db.export_data("FactSales")
+# FILE_PATH = 'Output_Data.csv'
+# list_column_names = ["order_id", "sale_date", "sale_time", "order_total_price", "order_details_id", "quantity", "total_price", "pizza_id", "pizza_category"]
+
+# db.export_to_csv(FILE_PATH=FILE_PATH, list_column_names=list_column_names, data_frame=demo_dataframe)
--- sqlServerConnection.py
+++ sqlServerConnection.py
@@ -2,7 +2,10 @@
 Connects to a SQL database using pyodbc
 """
 import pyodbc
+import csv
 import pandas as pd
+from pyodbc import connect
+
 
 class SqlServerConnection:
     DRIVER = ""
@@ -25,8 +28,50 @@ class SqlServerConnection:
         print('Connection establish')
         return connection
 
-    # Import du lieu
-    def import_data(self, df, table_name, column_names=None):
+    # Convert import data to dataframe
+    def import_data_to_dataframe(self, RAW_DATAFRAME, tuple_dataframe_columns, the_firts_columns_is_null=False,
+                                 accept_null=False, column_accept_null=None, set_zero=False, column_set_zero=None):
+        if not RAW_DATAFRAME.empty and tuple_dataframe_columns:
+            columns_dataframe = []
+            first_col = tuple_dataframe_columns[0]
+            # Xử lý cột đầu tiên
+            if the_firts_columns_is_null and first_col not in RAW_DATAFRAME.columns:
+                columns_dataframe.append(pd.Series(range(1, len(RAW_DATAFRAME) + 1), name=first_col))
+            elif first_col in RAW_DATAFRAME.columns and not RAW_DATAFRAME[first_col].isnull().all():
+                columns_dataframe.append(RAW_DATAFRAME[first_col])
+            else:
+                print(f"Warning: Column '{first_col}' not found or contains only NaN values.")
+                return None
+            # Thêm các cột còn lại
+            for col in tuple_dataframe_columns[1:]:
+                if col in RAW_DATAFRAME.columns:
+                    if set_zero and col == column_set_zero:
+                        columns_dataframe.append(pd.Series([0] * len(RAW_DATAFRAME), name=col))  # Set cột bằng 0
+                    elif accept_null and col == column_accept_null:
+                        columns_dataframe.append(RAW_DATAFRAME[col])  # Chấp nhận Null
+                    elif not RAW_DATAFRAME[col].isnull().all():
+                        columns_dataframe.append(RAW_DATAFRAME[col])
+                    else:
+                        print(f"Warning: Column '{col}' contains only NaN values.")
+                else:
+                    if accept_null and col == column_accept_null:
+                        columns_dataframe.append(
+                            pd.Series([None] * len(RAW_DATAFRAME), name=col))  # Cột không tồn tại, set Null
+                    else:
+                        print(f"Warning: Column '{col}' not found.")
+            # Tạo DataFrame
+            if columns_dataframe:
+                return pd.concat(columns_dataframe, axis=1)
+            else:
+                print("Error: No valid columns to create DataFrame.")
+                return None
+        else:
+            print("DataFrame is empty or tuple_dataframe_columns is empty.")
+            return None
+
+
+    # Import data to sql
+    def import_data_to_sql(self, df, table_name, column_names=None):
         conn = self.connection()
         cursor = conn.cursor()
 
@@ -50,6 +95,34 @@ class SqlServerConnection:
         conn.commit()
         cursor.close()
         conn.close()
+
+    # Lấy dữ liệu từ CSDL
+    def export_data (self, table_name):
+        conn = self.connection()
+        cursor = conn.cursor()
+
+        # Viet lenh lay lay ra cac gia tri trong bang
+        sql = f"select * from {table_name}"
+        cursor.execute(sql)
+
+        rows = cursor.fetchall()
+        cursor.close()
+        conn.close()
+
+        return rows
+
+    # Nhap du lieu vào file csv
+    def export_to_csv (self, FILE_PATH, list_column_names, data_frame):
+        if data_frame:
+            with open(f'{FILE_PATH}', 'w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(list_column_names)
+                for row in data_frame:
+                    list_element = []
+                    for element in row:
+                        list_element.append(element)
+                    writer.writerow(list_element)
+
 
 # Test
 # Giả sử pizza_categories là DataFrame của bạn
